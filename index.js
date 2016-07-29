c87b07e5947c07bbaf20e424f80753a4e349b794
Christoph Werner <christoph@codepunkt.de>
2016-07-29 19:26:09 +0200

    Log pokemon without pokemon_id

--- index.js
+++ index.js
@@ -101,7 +101,9 @@ new PokemonGoMITM({port: 8081})
       for (var i = 0, len = ref.length; i < len; i++) {
         var pokemon = ref[i].inventory_item_data.pokemon_data;
         if (pokemon) {
-          console.log(pokemon.pokemon_id)
+          if (!pokemon.pokemon_id) {
+            console.log(pokemon)
+          }
           iv = ((pokemon.individual_attack || 0) + (pokemon.individual_defense || 0) + (pokemon.individual_stamina || 0)) / 45.0 * 100;
           iv = Math.floor(iv * 10) / 10;
           // var shortened = pokemon.pokemon_id.substr(0, 8);
